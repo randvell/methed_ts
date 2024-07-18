bfb54563275394a4959df19665095749916151ec
Nikita Zimin <n.zimin@aristos.pw>
2024-07-18 18:41:00 +0300

    Module 10 Lesson 5

--- src/task4.ts
+++ src/task4.ts
@@ -1,57 +1,59 @@
-interface User {
-  type: 'user';
-  name: string;
-  age: number;
-  group: string;
-}
-
-interface Admin {
-  type: 'admin';
-  name: string;
-  age: number;
-  role: string;
-}
-
-type Person = User | Admin;
-
-const persons: Person[] = [
-  {
-    type: 'admin',
-    name: 'Иван Петров',
-    age: 27,
-    role: 'Administrator',
-  },
-  {
-    type: 'user',
-    name: 'Марат Aляуддинов',
-    age: 20,
-    group: 'музыкант',
-  },
-];
-
-const isAdmin = (person: Person): person is Admin => {
-  return person.type === 'admin';
-};
-
-const isUser = (person: Person): person is User => {
-  return person.type === 'user';
-};
-
-const logPerson = (person: Person) => {
-  let information: string = '';
-  if (isAdmin(person)) {
-    information = person.role;
+{
+  interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    group: string;
   }
-  if (isUser(person)) {
-    information = person.group;
+
+  interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
   }
-  console.log(` - ${person.name}, ${person.age}, ${information}`);
-};
 
-console.log('Admins:');
-persons.filter(isAdmin).forEach(logPerson);
+  type Person = User | Admin;
 
-console.log();
+  const persons: Person[] = [
+    {
+      type: 'admin',
+      name: 'Иван Петров',
+      age: 27,
+      role: 'Administrator',
+    },
+    {
+      type: 'user',
+      name: 'Марат Aляуддинов',
+      age: 20,
+      group: 'музыкант',
+    },
+  ];
 
-console.log('Users:');
-persons.filter(isUser).forEach(logPerson);
+  const isAdmin = (person: Person): person is Admin => {
+    return person.type === 'admin';
+  };
+
+  const isUser = (person: Person): person is User => {
+    return person.type === 'user';
+  };
+
+  const logPerson = (person: Person) => {
+    let information: string = '';
+    if (isAdmin(person)) {
+      information = person.role;
+    }
+    if (isUser(person)) {
+      information = person.group;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${information}`);
+  };
+
+  console.log('Admins:');
+  persons.filter(isAdmin).forEach(logPerson);
+
+  console.log();
+
+  console.log('Users:');
+  persons.filter(isUser).forEach(logPerson);
+}
---
+++ src/task5.ts
@@ -0,0 +1,85 @@
+interface User {
+  type: 'user';
+  name: string;
+  age: number;
+  group: string;
+}
+
+interface Admin {
+  type: 'admin';
+  name: string;
+  age: number;
+  role: string;
+}
+
+type Person = User | Admin;
+
+const persons: Person[] = [
+  {
+    type: 'admin',
+    name: 'Иван Петров',
+    age: 27,
+    role: 'Administrator',
+  },
+  {
+    type: 'user',
+    name: 'Марат Aляуддинов',
+    age: 20,
+    group: 'музыкант',
+  },
+  {
+    type: 'user',
+    name: 'Екатерина Попова',
+    age: 24,
+    group: 'дизайнер сайтов',
+  },
+  {
+    type: 'admin',
+    name: 'Аркадий Паравозов',
+    age: 55,
+    role: 'Системный администратор',
+  },
+  {
+    type: 'user',
+    name: 'Даня Поперечный',
+    age: 28,
+    group: 'Комик',
+  },
+  {
+    type: 'admin',
+    name: 'Олег',
+    age: 44,
+    role: 'Модератор',
+  },
+];
+
+const isAdmin = (person: Person): person is Admin => person.type === 'admin';
+const isUser = (person: Person): person is User => person.type === 'user';
+
+const logPerson = (person: Person) => {
+  let information = '';
+
+  if (isAdmin(person)) {
+    information = person.role;
+  }
+
+  if (isUser(person)) {
+    information = person.group;
+  }
+
+  console.log(` - ${person.name}, ${person.age}, ${information}`);
+};
+
+const filterUsers = (persons: Person[], criteria: Partial<User>): User[] =>
+  persons.filter(isUser).filter((user) => {
+    const criteriaKeys = Object.keys(criteria) as (keyof User)[];
+    return criteriaKeys.every(
+      (fieldName) => user[fieldName] === criteria[fieldName]
+    );
+  });
+
+console.log('Users of age 24:');
+
+filterUsers(persons, {
+  age: 24,
+}).forEach(logPerson);
